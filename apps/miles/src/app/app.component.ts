96e115559b686aaf60e0749b67792f765cdc9078
mikezks <23002661+mikezks@users.noreply.github.com>
2024-02-13 16:46:51 +0100

    init: miles app

---
+++ apps/miles/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+   standalone: true,
+   imports: [],
+   selector: 'flight-demo-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+   title = 'miles';
+}
+
+export default AppComponent;
